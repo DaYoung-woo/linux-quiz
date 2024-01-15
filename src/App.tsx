824eeac38df58a0920f3060f85bf72b7bf05bbcd
DaYoung-woo <iamwooda0@gmail.com>
2024-01-15 14:45:18 +0900

    ThemeContext 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import logo from "./assets/img/logo.svg";
 import logoDark from "./assets/img/logo_dark.svg";
+import { ThemeContext } from "./context/ThemeContext";
+
 function App() {
   const [show, setShow] = useState(false);
+  const [theme, setTheme] = useState("light");
 
   setTimeout(() => setShow(true), 2000);
   return (
     <div className="App">
-      {!show && <img src={logo} alt="logo" />}
-      {!show && <img src={logoDark} alt="logo" />}
-      {show && <Outlet />}
+      <ThemeContext.Provider value={theme}>
+        {!show && theme && <img src={logo} alt="logo" />}
+        {!show && !theme && <img src={logoDark} alt="logo" />}
+        {show && <Outlet />}
+      </ThemeContext.Provider>
     </div>
   );
 }
