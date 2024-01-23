5a84b8459b2139efc3aa8d5735eb883680620802
DaYoung-woo <iamwooda0@gmail.com>
2024-01-23 18:00:47 +0900

    문제 풀이 페이지 버튼 active 설정

--- src/views/QuizForm.tsx
+++ src/views/QuizForm.tsx
@@ -1,8 +1,10 @@
 import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { quizListApi } from "../api/api";
+import { useState } from "react";
 
 function QuizForm() {
+  const [answer, setAnswer] = useState(null);
   const [searchParams] = useSearchParams();
   const year = searchParams.get("year");
   const order = searchParams.get("order");
@@ -25,24 +27,47 @@ function QuizForm() {
     <div>loading...</div>
   ) : (
     data !== null && (
-      <div className="m-4">
+      <div className="m-4 mt-8">
         <h3 className="font-medium">{data.title}</h3>
-        <div className="py-4">
-          <div className="border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md">
+        <div className="pt-6 pb-4">
+          <button
+            className={`border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md w-full ${
+              answer === 1 && "border-slate-500"
+            }`}
+            onClick={() => setAnswer(1)}
+          >
             {data.distractor1}
-          </div>
-          <div className="border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md">
+          </button>
+          <button
+            className={`border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md w-full ${
+              answer === 2 && "border-slate-500"
+            }`}
+            onClick={() => setAnswer(2)}
+          >
             {data.distractor2}
-          </div>
-          <div className="border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md">
+          </button>
+          <button
+            className={`border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md w-full ${
+              answer === 3 && "border-slate-500"
+            }`}
+            onClick={() => setAnswer(3)}
+          >
             {data.distractor3}
-          </div>
-          <div className="border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md">
+          </button>
+          <button
+            className={`border border-slate-200 px-3 py-2 my-1 flex justify-between rounded-md w-full ${
+              answer === 4 && "border-slate-500"
+            }`}
+            onClick={() => setAnswer(4)}
+          >
             {data.distractor4}
-          </div>
+          </button>
         </div>
 
-        <button className="w-full text-center py-2 bg-indigo-500 rounded-md text-slate-50 disabled:bg-slate-200 disabled:text-slate-400">
+        <button
+          disabled={!answer}
+          className="w-full text-center py-2 bg-indigo-500 rounded-md text-slate-50 disabled:bg-slate-200 disabled:text-slate-400"
+        >
           SUBMIT
         </button>
       </div>
