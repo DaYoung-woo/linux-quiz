ecd6126900e37ee5ce9b5b10ca433202286a24ce
DaYoung-woo <iamwooda0@gmail.com>
2024-01-24 18:04:43 +0900

    카테고리 리스트 추가

--- src/views/admin/QuizList.tsx
+++ src/views/admin/QuizList.tsx
@@ -5,25 +5,48 @@ import { ReactComponent as Plus } from "../../assets/img/plus.svg";
 import { ReactComponent as Trashbin } from "../../assets/img/trashbin.svg";
 import { useQuery } from "@tanstack/react-query";
 import { categoryListApi } from "../../api/api";
+import { useEffect, useState } from "react";
 
 function Quiz() {
   const quizListLoadable = useRecoilValueLoadable(adminQuizListAtom);
+  const [category, setCategory] = useState("");
 
   // 카테고리 리스트 api 요청
-  useQuery({
+  const { status, data } = useQuery({
     queryKey: ["fetchCategoryList"],
     queryFn: () => categoryListApi(),
   });
 
+  useEffect(() => {
+    if (data.length) setCategory(data[0].id);
+  }, [data]);
+
+  useEffect(() => {}, [category]);
   return (
     <div>
       <h1>문제 관리</h1>
       <div className="flex justify-between mt-5 mb-2">
-        <input
-          type="text"
-          className="bg-slate-50 rounded-full border px-5 active:border-stone-100 py-2"
-          placeholder="검색"
-        />
+        <select
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+          className="mr-4 border rounded-full px-2 py-1 w-40"
+        >
+          <option value="" disabled>
+            카테고리
+          </option>
+          {status === "pending" && (
+            <option value="loading">...로딩중입니다.</option>
+          )}
+          {status === "success" &&
+            data.map(({ year, order, id }) => (
+              <option value={id} key={id}>
+                {year}년도 {order}회차
+              </option>
+            ))}
+        </select>
+
         <Link to="/admin/quiz_form">
           <button className="flex items-center pl-7 pr-8 py-2 bg-indigo-500 rounded-md text-slate-50">
             <Plus fill="white" />
